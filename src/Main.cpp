56704f21291d76c75019cbed7fe512a48a9e4715
Kareem Skinner <kareem@karsteski.com>
2022-08-28 19:41:41 -0400

    Repo init

---
+++ src/Main.cpp
@@ -0,0 +1,19 @@
+// Maths Library
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+// DearImGUI
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, Renderex." << std::endl;
+}
